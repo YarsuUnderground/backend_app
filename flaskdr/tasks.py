2bea9eaf57350cd0c0dc81ceca8130c51db30f79
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-10-17 15:28:12 +0300

    modified tasks.py and user.py

--- flaskdr/tasks.py
+++ flaskdr/tasks.py
@@ -5,25 +5,41 @@ from bson import json_util
 from bson.objectid import ObjectId
 import functools,json,os, hashlib
 from . import database
+from datetime import datetime
 
 ta = Blueprint('/tasks',__name__, url_prefix='/tasks')
 
 @ta.route('/create/', methods=['GET', 'POST'])
 def create_task():
     data = request.get_json(silent = True)
+    if data is None:
+        data = request.args
     #number = data.get('number')
     title = data.get('title')
+    description = data.get('description')
+    deadline = requests.get('deadline') # to datetime.datetime?
+    executors = data.get('executors')
+    tasks_col = database.get_db_connection()[database.TASKS_COLLECTION_NAME]
+    #task = {}
+    task = {'number': number, 'title': title, 'description':description, 'deadline': datetime.now(), 'executors': executors }
+    tasks_col.insert_one(task)
 
-
-@ta.route('/delete/', methods=['DELETE'])
+@ta.route('/delete/', methods=['GET', 'DELETE'])
 def delete_task():
-    pass
+    data = request.get_json(silent = True)
+    if data is None:
+        data = request.args
+    id = request.get('id')
+    tasks_col = database.get_db_connection()[database.TASKS_COLLECTION_NAME]
+    tasks_col.delete_one({'_id':ObjectId(id)})
+    return Response(status = 200)
 
-@ta.route('/update/', methods=['UPDATE'])
+@ta.route('/update/', methods=['GET', 'UPDATE'])
 def update_task():
     pass
 
 @ta.route('/all/', methods=['GET'])
 def create_task():
-    data = request.get_json(silent = True)
+    tasks_col = database.get_db_connection()[database.TASKS_COLLECTION_NAME]
+    jsonify(task_col)
     #number =  
--- flaskdr/user.py
+++ flaskdr/user.py
@@ -8,29 +8,29 @@ from . import database
 
 us =  Blueprint('/user', __name__, url_prefix='/user')
 
-@us.route('/user_type/<token>/', methods=['GET', 'POST'])
-def get_type(token):
-    #token = request.args.get('token')
+@us.route('/user_type/', methods=['GET', 'POST'])
+def get_type():
+    token = request.args.get('token')
     users_col = database.get_db_connection()[database.USERS_COLLECTION_NAME]
     return jsonify('isAdmin': users_col.find_one({'token':token})['isAdmin'])
 
-@us.route('/user_id/<token>/', methods=['GET', 'POST'])
-def get_id(token):
-    #token = request.args.get('token')
+@us.route('/user_id/', methods=['GET', 'POST'])
+def get_id():
+    token = request.args.get('token')
     users_col = database.get_db_connection()[database.USERS_COLLECTION_NAME]
-    return jsonify('id': users_col.find_one({'token':token})['_id'].toString())
+    return jsonify('id': str(users_col.find_one({'token':token})['_id']))
 
-@us.route('/user_data/<id>/', methods=['GET', 'POST'])
-def get_user(id):
-    #id = request.args.get('id')
+@us.route('/user_data/', methods=['GET', 'POST'])
+def get_user():
+    token = request.args.get('token')
     users_col = database.get_db_connection()[database.USERS_COLLECTION_NAME]
-    doc = users_col.find_one({'_id':id}) 
+    doc = users_col.find_one({'token': token}) 
     data = {"first_name":doc['first_name'],"second_name":doc['last_name'],"isAdmin":doc['isAdmin'],"email":doc['login'],"phone":doc['phone']}
     return jsonify(data)
 
-@us.route('/user_tasks/<token>', methods=['GET', 'POST'])
+@us.route('/user_tasks/', methods=['GET', 'POST'])
 def get_user_tasks(token):
-    #token = request.args.get('token')
+    token = request.args.get('token')
     users_col = database.get_db_connection()[database.USERS_COLLECTION_NAME]
     user = users_col.find_one({'token':token})
     return jsonify(user['tasks'])
