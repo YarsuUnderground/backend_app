15aa89c69d5d343890860df680f041ba0a4cde7f
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-10-17 23:31:37 +0300

    helper

--- flaskdr/helper.py
+++ flaskdr/helper.py
@@ -2,7 +2,9 @@ import copy
 def get_next_id(collection, id_col_name):
     if collection.count() == 0:
         return 1
-    return collection.find().sort(id_col_name,-1).limit(1)['_id'] + 1
+    docs = collection.find().sort(id_col_name,-1).limit(1)
+    for doc in docs:
+        print doc 
 
 def get_next_subtask_id(subtasks):
     if len(subtasks) == 0:
