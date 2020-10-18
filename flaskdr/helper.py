f8d1e8afc8bb15ef7f00e1461614d038d4717fe3
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-10-18 06:17:12 +0300

    frgre

--- flaskdr/helper.py
+++ flaskdr/helper.py
@@ -6,7 +6,3 @@ def get_next_id(collection, id_col_name):
     for doc in docs:
         return doc[id_col_name] + 1
 
-def get_next_subtask_id(subtasks):
-    if len(subtasks) == 0:
-        return 1
-    return copy.deepcopy(subtasks).sort()[-1] + 1.
