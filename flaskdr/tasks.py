b1d8c1f092b6988b4a7996ee7c46551dd50e73ba
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-10-18 06:40:05 +0300

    frgre

--- flaskdr/tasks.py
+++ flaskdr/tasks.py
@@ -95,6 +95,7 @@ def delete_subtask():
     tasks_col = database.get_db_connection()[database.TASKS_COLLECTION_NAME]
     subtasks_col = database.get_db_connection()[database.SUBTASKS_COLLECTION_NAME]
     #tasks_col.update({},{"$pull":{'subtasks':subtask_id}})
+    performers = subtasks_col.find_one({'_id':int(subtask_id)})['performers']
     subtasks_col.delete_one({'_id':int(subtask_id)})
     notify_executors(database.get_db_connection()[database.USERS_COLLECTION_NAME],subtask_id,performers, action="$pull",category="subtasks")
     return Response(status = 200)
